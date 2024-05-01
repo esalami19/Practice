b9e2f5c137ee376b7adc61b72532047cdbc1d28a
User <esalami19@yahoo.co.uk>
2024-05-02 00:59:16 +0100

    DOM workshop day 3/100

---
+++ DOM/index.js
@@ -0,0 +1,5 @@
+document.body.style.backgroundColor = "hsl(0, 0%, 15%)"
+const message = " we can do this thang"
+const motivateMessage = document.getElementById("title")
+//to change message of elemnt
+motivateMessage.textContent += message === ""? " we've got this": message
