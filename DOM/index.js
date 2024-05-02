4839b134b1891e221b9ca69c76eb9d5791539541
User <esalami19@yahoo.co.uk>
2024-05-02 01:02:42 +0100

    DOM workshop - day 3/100

--- DOM/index.js
+++ DOM/index.js
@@ -1,5 +1,5 @@
 document.body.style.backgroundColor = "hsl(0, 0%, 15%)"
 const message = " we can do this thang"
 const motivateMessage = document.getElementById("title")
-//to change message of elemnt
+//to change message of elemnt w/ ternary operator
 motivateMessage.textContent += message === ""? " we've got this": message
